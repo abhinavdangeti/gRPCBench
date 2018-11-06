5a2e835fa428ca32175336ca851d3e1719fd0191
Abhinav Dangeti <abhinavdangeti@users.noreply.github.com>
2018-11-06 15:28:55 -0800

    Adding gRPC server, client

---
+++ client/main.go
@@ -0,0 +1,129 @@
+//
+// gRPC: Benchmarks/Tests
+// Author: Abhinav Dangeti
+//
+
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+
+	pb "github.com/abhinavdangeti/gRPCBench/protobuf"
+	"github.com/jamiealquiza/tachymeter"
+	"google.golang.org/grpc"
+)
+
+type client struct {
+	name      string
+	addresses []string
+	t         *tachymeter.Tachymeter
+}
+
+func newClient(name string, addresses []string) *client {
+	return &client{
+		name:      name,
+		addresses: addresses,
+		t:         tachymeter.New(&tachymeter.Config{Size: 500}),
+	}
+}
+
+func (c *client) run() {
+	var wg sync.WaitGroup
+	start := time.Now()
+	for _, addr := range c.addresses {
+		wg.Add(1)
+		go func(address string) {
+			defer wg.Done()
+
+			name := c.name + "_" + address
+
+			// Set up a connection to the server.
+			conn, err := grpc.Dial(address, grpc.WithInsecure())
+			if err != nil {
+				log.Fatalf("[%v] did not connect: %v", name, err)
+			}
+			defer conn.Close()
+			co := pb.NewEngageClient(conn)
+
+			// Contact the server and print out its response.
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			r, err := co.Greet(ctx, &pb.Greeting{Name: name, Msg: "hi"})
+			if err != nil {
+				log.Fatalf("[%v] could not greet: %v", name, err)
+			} else if r.Msg != "hello " + name {
+				log.Fatalf("[%v] unexpected response: %v", name, r.Msg)
+			}
+
+			stream, err := co.ShipData(ctx, &pb.Request{Name: name, Ask: 10})
+			if err != nil {
+				log.Fatalf("[%v] could not request data to be shipped: %v", name, err)
+			}
+			for {
+				resp, err := stream.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil || resp == nil{
+					log.Fatalf("[%v] could not receive data: %v", name, err)
+				}
+			}
+
+			r, err = co.Greet(ctx, &pb.Greeting{Name: name, Msg: "bye"})
+			if err != nil {
+				log.Fatalf("[%v] could not complete: %v", name, err)
+			} else if r.Msg != "goodbye " + name {
+				log.Fatalf("[%v] unexpected response: %v", name, r.Msg)
+			}
+		}(addr)
+	}
+	wg.Wait()
+	// Tachymeter is thread-safe.
+	c.t.AddTime(time.Since(start))
+}
+
+func (c *client) results() {
+	log.Printf("\n===================================\n" +
+		"Client: %v\n" +
+		"Addresses: %v\n" +
+		"%v\n" +
+		"===================================\n",
+		c.name, c.addresses, c.t.Calc().String())
+}
+
+func main() {
+	var clients []*client
+	for i, addresses := range [][]string{
+		[]string{"127.0.0.1:12345", "127.0.0.1:23456"},
+		[]string{"127.0.0.1:23456", "127.0.0.1:34567"},
+		[]string{"127.0.0.1:34567", "127.0.0.1:12345"}} {
+		clients = append(clients, newClient("client"+strconv.Itoa(i), addresses))
+	}
+
+	start := time.Now()
+	for {
+		var wg sync.WaitGroup
+		for _, cl := range clients {
+			wg.Add(1)
+			go func(c *client) {
+				c.run()
+				wg.Done()
+			}(cl)
+		}
+		wg.Wait()
+
+		if time.Since(start) > 20*time.Second {
+			break
+		}
+	}
+
+	for _, cl := range clients {
+		cl.results()
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,121 @@
+//
+// gRPC: Benchmarks/Tests
+// Author: Abhinav Dangeti
+//
+// protoc -I protobuf/ protobuf/engage.proto --go_out=plugins=grpc:protobuf
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	pb "github.com/abhinavdangeti/gRPCBench/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var greetings map[string]string
+
+func init() {
+	// initialize greetings
+	greetings = make(map[string]string)
+	greetings["hi"] = "hello"
+	greetings["bye"] = "goodbye"
+}
+
+var sample_json_data = string(`{
+	"status":{
+		"total":1,
+		"failed":0,
+		"successful":1,
+		"errors":{}
+	},
+	"request":{
+		"query":{
+			"query":"blah blah"
+		},
+		"size":10,
+		"from":0,
+		"highlight":null,
+		"fields":[],
+		"facets":null,
+		"explain":false
+	},
+	"hits":[
+		{
+			"id": "doc_1"
+		},
+		{
+			"id": "doc_2"
+		},
+		{
+			"id": "doc_3"
+		}
+	],
+	"total_hits":3,
+	"max_score":0,
+	"took":0,
+	"facets":null
+}`)
+
+type server struct{
+	name string
+}
+
+func (s *server) Greet(ctx context.Context, in *pb.Greeting) (*pb.Greeting, error) {
+	if in == nil {
+		return nil, fmt.Errorf("-no greeting-")
+	}
+
+	reply := "didn't recoginize that,"
+	if v, exists := greetings[in.Msg]; exists {
+		reply = v
+	}
+
+	return &pb.Greeting{Name: s.name, Msg: reply + " " + in.Name}, nil
+}
+
+func (s *server) IdentifyData(stream pb.Engage_IdentifyDataServer) error {
+
+	return nil
+}
+
+func (s *server) ShipData(req *pb.Request, stream pb.Engage_ShipDataServer) error {
+	if req == nil {
+		return fmt.Errorf("-no request-")
+	}
+	for i := 0; i < int(req.Ask); i++ {
+		resp := &pb.Response{Name: s.name, Content: sample_json_data}
+		if err := stream.Send(resp); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for _, val := range []string{":12345", ":23456", ":34567"} {
+		wg.Add(1)
+		go func(port string){
+			defer wg.Done()
+			listen, err := net.Listen("tcp", port)
+			if err != nil {
+				log.Fatalf("Failed to listen: %v", err)
+			}
+			s := grpc.NewServer()
+			pb.RegisterEngageServer(s, &server{name: "server" + port})
+			// register reflection service on gRPC server.
+			reflection.Register(s)
+			log.Printf("Serving %v", port)
+			if err = s.Serve(listen); err != nil {
+				log.Fatalf("Failed to serve: %v", err)
+			}
+		}(val)
+	}
+	wg.Wait()
+}
